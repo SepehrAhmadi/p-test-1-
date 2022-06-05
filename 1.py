2a9af2c5c78767fda608aaf9e13860372d02c219
sepehr <ahmadisepehr75@gmail.com>
2022-06-05 19:10:52 +0430

    import random and list changed

--- 1.py
+++ 1.py
@@ -1,3 +1,5 @@
+import random
+
 print("HI")
 for i in range(0, 3):
     print("Hello")
@@ -6,4 +8,6 @@ for i in range(0, 3):
 print("Enter your name: ")
 print("Enter your family: ")
 
-list = [1, 2, 3]
+list = [4, 5, 6, 7]
+print(list)
+
